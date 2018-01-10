bfb6e057c68a96d6df253157ef83ec8d8d38ed0b
Isaiah Martinez <imartinez32@cnm.edu>
2018-01-10 08:52:34 -0700

    added index file with data

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang=”en-US”>
+	<head>
+		<meta charset="UTF-8">
+		<title>Snap Tables</title>
+		<link rel="stylesheet" href="./styles/styles.css">
+	</head>
+	<body>
+		<h1>Web Languages</h1>
+		<table>
+			<tr>
+				<th>Language</th>
+				<th>Number of Users</th>
+				<th>Downside</th>
+			</tr>
+			<tr>
+				<td>Java</td>
+				<td>125,718,483</td>
+				<td>Frameworks revolt! It's Java Spring!!</td>
+			</tr>
+			<tr>
+				<td>.NET</td>
+				<td>64,371,473</td>
+				<td>PHP forces to clean twice per week</td>
+			</tr>
+			<tr>
+				<td>PHP</td>
+				<td>75,383,142</td>
+				<td>Misunderstood by Java and .NET Developers</td>
+			</tr>
+			<tr>
+				<td>Ruby</td>
+				<td>1,403</td>
+				<td>Only for Hipster Driven Development (HDD)</td>
+			</tr>
+		</table>
+	</body>
+</html>
